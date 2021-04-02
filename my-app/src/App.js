7946c4a458e65c1fe03a4d998349e54f7251fe44
janecyyu <janecyyu@gmail.com>
2021-04-02 16:49:56 -0700

    set up data

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import VaxGrid from "./components/VaxGrid";
+
+function App() {
+  return (
+    <div className="App">
+      <VaxGrid />
+    </div>
+  );
+}
+
+export default App;
