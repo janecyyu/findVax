bf8fa95a7c7433c5b39ff188519c2678e75eb006
janecyyu <janecyyu@gmail.com>
2021-04-12 14:26:42 -0700

    header ok

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Iframe from "react-iframe";
+import TwitterIcon from "@material-ui/icons/Twitter";
 
 export default function Header() {
   return (
@@ -8,6 +10,23 @@ export default function Header() {
         VaxBay
       </Link>
       <p className="sub-title">A Vaccine Tracker in Bay Area, CA</p>
+      <div className="menu">
+        <a
+          className="twitter"
+          href="https://twitter.com/VaxBay?ref_src=twsrc%5Etfw"
+        >
+          <TwitterIcon style={{ color: "rgb(58, 154, 169)" }} />
+        </a>
+        <div className="about-container">
+          <Link className="about" to="/">
+            Home
+          </Link>
+          {" | "}
+          <Link className="about" to="/about">
+            About
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
