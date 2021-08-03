e025be89e7faa816ff78fcf4d5021fd24002296e
janecyyu <janecyyu@gmail.com>
2021-08-03 13:40:02 -0700

    add notes at app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,8 @@ import Header from "./components/Header";
 import Disclaimer from "./components/Disclaimer";
 import Footer from "./components/Footer";
 
+// Because more and more availabilities come out, the list maybe longer and longer, will add a filter feature in the future for a better experience.
+
 function App() {
   return (
     <div className="App">
